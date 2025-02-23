08ae8e5da085d029f565c147d5412c28423ce40a
ItalianodeCombate <popinmarco@gmail.com>
2025-02-23 20:00:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const Discord = require('discord.js');
 const client = new Discord.Client({ intents: ["Guilds", "GuildMessages", "MessageContent"] });
 
+const token = process.env.DISCORD_TOKEN;
+
 client.on('ready', () => {
-    console.log(`Bot logged in as ${client.user.tag}!`);
-    client.user.setActivity('Próximamente...', { type: 'PLAYING' });
+  console.log(`Bot logged in as ${client.user.tag}!`);
+
+  // Establecer el estado del bot
+  client.user.setActivity('Próximamente...', { type: 'PLAYING' }); // Puedes cambiar 'PLAYING' por otro tipo
+
+  //Otras opciones de tipo son:
+  //LISTENING: Escuchando
+  //WATCHING: Viendo
+  //COMPETING: Compitiendo
+  //STREAMING: Transmitiendo.
 });
 
 client.on('messageCreate', (message) => {
-    if (message.content.startsWith('/say')) {
-        const text = message.content.slice(5).trim();
-        if (text) {
-            message.channel.send(text);
-        } else {
-            message.reply('Debes proporcionar un texto después de /say.');
-        }
-    } else if (message.content === '!ip') {
-        message.channel.send(ipMessage);
-    }
+  if (message.content === 'ping') {
+    message.reply('pong!');
+  }
 });
 
-client.login(DISCORD_TOKEN);
+client.login(token);
