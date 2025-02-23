d195f21dfb06e14e400301ad85c80173087ffb69
ItalianodeCombate <popinmarco@gmail.com>
2025-02-23 19:56:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client({ intents: ["Guilds", "GuildMessages", "MessageContent"] });
 
-const token = process.env.DISCORD_TOKEN;
-const ipMessage = '<:pepe_happy:1232008882935693322> **armaklandia.hidenmc.com**\n1.8 - 1.21.4'; // Mensaje con emoji e IP
-
 client.on('ready', () => {
     console.log(`Bot logged in as ${client.user.tag}!`);
     client.user.setActivity('Próximamente...', { type: 'PLAYING' });
