735011c69558431f12601b67fa22531347ca7970
ItalianodeCombate <popinmarco@gmail.com>
2025-03-04 17:43:48 +0100

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -5,6 +5,11 @@ app.get('/', (req, res) => {
   res.send('El bot está online!');
 });
 
-app.listen(3000, () => {
-  console.log('Servidor iniciado en el puerto 3000');
-});
+function keepAlive() {
+  app.listen(3000, () => {
+    console.log('Servidor iniciado en el puerto 3000');
+  });
+}
+
+module.exports = { keepAlive };  // Exportar la función para que pueda ser llamada
+
